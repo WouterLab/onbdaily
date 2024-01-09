2b8ffa10c2c76e3f3b3134d147a880c897464593
WouterLab <panov135@mail.ru>
2024-01-09 15:01:09 +0300

    fix

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://onbdaily.na4u.ru",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://onbdaily.na4u.ru",
+  // baseURL: "http://localhost:4000",
 });
 
 export default instance;
